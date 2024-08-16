8d12393cd02d28aaf8d10a7269e9172570c5f706
Kimmingki <rlaalsrl9569@naver.com>
2024-08-16 14:06:47 +0900

    파라미터 바인딩

--- src/main/java/study/data_jpa/repository/MemberRepository.java
+++ src/main/java/study/data_jpa/repository/MemberRepository.java
@@ -23,4 +23,7 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     @Query("select new study.data_jpa.dto.MemberDTO(m.id, m.username, t.name) from Member m join m.team t")
     List<MemberDTO> findMemberDTO();
+
+    @Query("select m from Member m where m.username in :names")
+    List<Member> findByNames(@Param("names") List<String> names);
 }
