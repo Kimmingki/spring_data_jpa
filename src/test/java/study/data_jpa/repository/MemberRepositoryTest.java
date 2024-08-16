70068aee47a04fe3a5e89d7fd4c18ebd47232692
Kimmingki <rlaalsrl9569@naver.com>
2024-08-16 09:31:15 +0900

    공통 인터페이스 적용

--- src/test/java/study/data_jpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/data_jpa/repository/MemberRepositoryTest.java
@@ -1,10 +1,14 @@
 package study.data_jpa.repository;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import study.data_jpa.entity.Member;
+import study.data_jpa.entity.Team;
+
+import java.util.List;
 
 @Transactional
 @SpringBootTest
@@ -12,6 +16,9 @@ class MemberRepositoryTest {
 
     @Autowired
     MemberRepository memberRepository;
+
+    @Autowired
+    TeamRepository teamRepository;
 
     @Test
     public void testMember() {
@@ -23,4 +30,34 @@ class MemberRepositoryTest {
 //        Assertions.assertThat(findMember).isEqualTo(member);
     }
 
+    @Test
+    public void basicCRUD() {
+        Team teamA = new Team("Team A");
+        teamRepository.save(teamA);
+
+        Member member1 = new Member("member1", 20, teamA);
+        Member member2 = new Member("member2", 28, teamA);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        // 단건 조회 검증
+        Member findMember1 = memberRepository.findById(member1.getId()).get();
+        Member findMember2 = memberRepository.findById(member2.getId()).get();
+        Assertions.assertThat(findMember1).isEqualTo(member1);
+        Assertions.assertThat(findMember2).isEqualTo(member2);
+
+        // 전체 조회 검증
+        List<Member> all = memberRepository.findAll();
+        Assertions.assertThat(all).hasSize(2);
+
+        // 카운팅 검증
+        long count = memberRepository.count();
+        Assertions.assertThat(count).isEqualTo(2);
+
+        // 삭제 검증
+        memberRepository.delete(findMember1);
+        memberRepository.delete(findMember2);
+        long deleteCount = memberRepository.count();
+        Assertions.assertThat(deleteCount).isEqualTo(0);
+    }
 }
