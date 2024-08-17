7b5bf437c95578f8f54c1b8d847c156f815b31d5
Kimmingki <rlaalsrl9569@naver.com>
2024-08-17 10:50:27 +0900

    Web 확장 - 페이징과 정렬

--- src/main/java/study/data_jpa/controller/MemberController.java
+++ src/main/java/study/data_jpa/controller/MemberController.java
@@ -2,9 +2,13 @@ package study.data_jpa.controller;
 
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import study.data_jpa.dto.MemberDTO;
 import study.data_jpa.entity.Member;
 import study.data_jpa.repository.MemberRepository;
 
@@ -25,8 +29,16 @@ public class MemberController {
         return member.getUsername();
     }
 
+    @GetMapping("/members")
+    public Page<MemberDTO> findMembers(@PageableDefault(size = 5) Pageable pageable) {
+        Page<Member> page = memberRepository.findAll(pageable);
+        return page.map(m -> new MemberDTO(m.getId(), m.getUsername()));
+    }
+
     @PostConstruct
     private void init() {
-        memberRepository.save(new Member("memberA", 24));
+        for (int i=0; i<50; i++) {
+            memberRepository.save(new Member("member" + i, i));
+        }
     }
 }
--- src/main/java/study/data_jpa/dto/MemberDTO.java
+++ src/main/java/study/data_jpa/dto/MemberDTO.java
@@ -1,6 +1,7 @@
 package study.data_jpa.dto;
 
 import lombok.Data;
+import study.data_jpa.entity.Member;
 
 @Data
 public class MemberDTO {
@@ -9,9 +10,21 @@ public class MemberDTO {
     private String username;
     private String teamName;
 
+    public MemberDTO(Long id, String username) {
+        this.id = id;
+        this.username = username;
+    }
+
     public MemberDTO(Long id, String username, String teamName) {
         this.id = id;
         this.username = username;
         this.teamName = teamName;
+    }
+
+    // DTO에서 Entity를 참조하는 것은 괜찮음
+    public MemberDTO(Member member) {
+        this.id = member.getId();
+        this.username = member.getUsername();
+        this.teamName = member.getTeam().getName();
     }
 }
